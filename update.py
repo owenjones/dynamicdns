759ce32350a9d0c1e634b7f2d4c59eaf384dbbe5
Owen Jones <owen@owenjones.net>
2022-08-02 19:35:42 +0100

    added output for systemd journal; updated path library used

--- update.py
+++ update.py
@@ -1,23 +1,24 @@
 # DynamicDNS
 # Reads current IP address from Home Assistant and then updates DNS records held by DigitalOcean.
 
-# TODO:
-# - add logging to this
-# - use better path library
-
-import os.path
+from pathlib import Path
 import yaml
 import requests
 import digitalocean
 
-with open("config.yml", "r") as f:
-    config = yaml.safe_load(f)
+if Path("config.yml").is_file():
+    with open("config.yml", "r") as f:
+        config = yaml.safe_load(f)
+
+else:
+    print("Configuration file doesn't exist")
+    exit(1)
 
 try:
     url = config["homeassistant"]["url"]
     sensor = config["homeassistant"]["sensor"]
 
-    ro = requests.get(
+    request = requests.get(
         f"{ url }/api/states/sensor.{ sensor }",
         headers={
             "Authorization": "Bearer " + config["homeassistant"]["token"],
@@ -25,33 +26,37 @@ try:
         },
     )
 
-except:
-    exit(0)
+except Exception as e:
+    print(f"Problem calling Home Assistant API: { e }")
+    exit(1)
 
 else:
-    json = ro.json()
+    json = request.json()
     ip = json["state"]
 
-if os.path.isfile("state"):
+if Path("state").is_file():
     with open("state", "r") as f:
         last = f.read()
         if ip == last:
+            print("IP address hasn't changed, exiting")
             exit(0)
 
+print("IP address has changed, updating DNS records")
 
 for set in config["digitalocean"]["domains"]:
     domain = config["digitalocean"]["domains"][set]
-    do = digitalocean.Domain(
+    api = digitalocean.Domain(
         token=config["digitalocean"]["token"], name=domain["domain"]
     )
-    rec = do.get_records()
+    records = api.get_records()
 
-    for r in rec:
-        if r.name in domain["records"]:
-            r.data = ip
-            r.save()
+    for record in records:
+        if record.name in domain["records"]:
+            record.data = ip
+            record.save()
 
-with open("state", "w") as ipcheck:
-    ipcheck.write(ip)
+with open("state", "w") as state:
+    state.write(ip)
 
+print("DNS records updated")
 exit(0)
